b66fb635c608bd8c621007d491ebe37823057be3
Christopher Pepper <p3p@p3psoft.co.uk>
2020-11-11 16:56:10 +0000

    minor workarounds

--- Marlin/src/core/serial.h
+++ Marlin/src/core/serial.h
@@ -310,8 +310,9 @@ void serial_echopair_PGM(PGM_P const s_P, unsigned int v);
 void serial_echopair_PGM(PGM_P const s_P, unsigned long v);
 inline void serial_echopair_PGM(PGM_P const s_P, uint8_t v) { serial_echopair_PGM(s_P, (int)v); }
 inline void serial_echopair_PGM(PGM_P const s_P, bool v)    { serial_echopair_PGM(s_P, (int)v); }
-inline void serial_echopair_PGM(PGM_P const s_P, void *v)   { serial_echopair_PGM(s_P, (int)v); }
-inline void serial_echopair_PGM(PGM_P const s_P, size_t v)   { serial_echopair_PGM(s_P, (int)v); }
+inline void serial_echopair_PGM(PGM_P const s_P, void *v)   { serial_echopair_PGM(s_P, (long int)v); }
+//inline void serial_echopair_PGM(PGM_P const s_P, size_t v)   { serial_echopair_PGM(s_P, (long int)v); }
+#warning This REAAALY NEEDS FIXED , this will work on linux, reenable to work on windows, dont want to use any OS specific code ..
 
 void serialprintPGM(PGM_P str);
 void serial_echo_start();
