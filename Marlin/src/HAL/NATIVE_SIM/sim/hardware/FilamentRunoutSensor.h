bb53e2c43d649753c7f23085ae7b2cc9b705dc85
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-11-14 22:08:13 -0300

    dont change gpio in ui thread

--- Marlin/src/HAL/NATIVE_SIM/sim/hardware/FilamentRunoutSensor.h
+++ Marlin/src/HAL/NATIVE_SIM/sim/hardware/FilamentRunoutSensor.h
@@ -6,18 +6,17 @@
 class FilamentRunoutSensor {
 public:
   FilamentRunoutSensor(pin_type runout_pin, bool runtout_trigger_value) : runout_pin(runout_pin), runtout_trigger_value(runtout_trigger_value) {
-    Gpio::pin_map[runout_pin].value = !runtout_trigger_value;
+    Gpio::attach(runout_pin, [this](GpioEvent& event){ this->interrupt(event); });
+  }
+
+  void interrupt(GpioEvent &ev) {
+    if (ev.pin_id == runout_pin && ev.event == GpioEvent::GET_VALUE) {
+      Gpio::pin_map[runout_pin].value = filament_present ? !runtout_trigger_value : runtout_trigger_value;
+    }
   }
 
   void ui_info_callback(UiWindow*) {
     ImGui::Checkbox("Filament Present ", (bool*)&filament_present);
-    if (filament_present) {
-      Gpio::pin_map[runout_pin].value = !runtout_trigger_value;
-
-    }
-    else {
-      Gpio::pin_map[runout_pin].value = runtout_trigger_value;
-    }
   }
 
 private:
